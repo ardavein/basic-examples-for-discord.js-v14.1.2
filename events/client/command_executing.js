6580a00dbdba463ca2afa58b45f8420ff890555f
Ardavein <baskayaaylin7@gmail.com>
2022-08-01 22:02:53 +0300

    Update command_executing.js

--- events/client/command_executing.js
+++ events/client/command_executing.js
@@ -5,6 +5,7 @@ module.exports = {
   }, run: async (client, message) => { // Recieving the <Client> from the main file named 'ardavein.js'. 
    let prefix = client.config.prefix; // Declaring the prefix from client.config.
    if (!message.content.startsWith(prefix)) return; // If it does not starts with prefix we returning it to nothing. (It will run what after return, if we don't type any code here it will do nothing) 
+   if (message.author.bot) return; // Checking if user is a bot or not if it is a bot it will return.
    let args = message.content.slice(prefix.length).trim().split(/ +/) // Declaring the arguments except the prefix slice() is deleting the prefix, trim() is deleting the spaces, split is making it as a array
    let cmd = args.shift().toLowerCase();  // Removing the command from arguments and making it lower case. (PING to ping)
    if (cmd.length == 0) return; // If they just type ! it will return.
