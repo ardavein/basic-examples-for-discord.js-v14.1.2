cdf2bcf1eb1ddf27f00f4defa032ee676ed800a6
Ardavein <57666610+ardavein@users.noreply.github.com>
2025-02-12 17:04:32 +0300

    Update ping.js

--- slash_commands/general/ping.js
+++ slash_commands/general/ping.js
@@ -1,9 +1,9 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Needed this module to create a slash command.
-module.exports = { // Declaring the command data.
-    data: new SlashCommandBuilder() // Creating a slash command.
-    .setName('ping') // Name of command.
-    .setDescription('A basic ping pong slash command.'), // Description of command.
-    run: async (client, interaction) => { // Declaring the client and interaction from the event 'interactionCreate'.
-       interaction.reply(`Websocket: ${client.ws.ping}ms.`); // Replies with discord api websocket ping.
+const { SlashCommandBuilder } = require('@discordjs/builders'); 
+module.exports = { 
+    data: new SlashCommandBuilder() 
+    .setName('ping')
+    .setDescription('A basic ping pong slash command.'), 
+    run: async (client, interaction) => {
+       interaction.reply(`Websocket: ${client.ws.ping}ms.`); 
     },
 };
