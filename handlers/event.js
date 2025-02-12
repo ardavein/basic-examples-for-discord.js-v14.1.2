14a4772028be004e1994b19cb354120275f77e6d
Ardavein <57666610+ardavein@users.noreply.github.com>
2025-02-12 17:03:12 +0300

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,15 +1,15 @@
-const { readdirSync } = require('fs'); // Recieving the readdirSync function from <'fs'> module.
+ const { readdirSync } = require('fs'); 
 module.exports = { 
-data: { // Declaring the handler data.
-name: 'event_handler' // It is not important for right now but if will it in sometime? Declaring the name of handler
-}, run: async (client) => { //Recieving the <Client> from the main file named './ardavein.js'. 
+data: {
+name: 'event_handler' 
+}, run: async (client) => { 
 
- readdirSync('./events/').forEach(dir => { // Getting every folders named like client, logging, eg.
-  let events = readdirSync(`./events/${dir}/`).filter(file => file.endsWith('.js')); // Declaring events files.
-  for (let event_file of events) { // There is a for function(?). It will works for every file in './events/{dir}/'
-  let event = require(`../events/${dir}/${event_file}`); // Declaring the event file to run it
-  if (event.once == true) client.once(event.data.name, (client, ...args) => event.run(client, ...args)); // If <Event>.once is true it will run for once.
-  else client.on(event.data.name, (... args) => event.run(client, ...args)); // If it is not works for every time it triggered 
+ readdirSync('./events/').forEach(dir => { 
+  let events = readdirSync(`./events/${dir}/`).filter(file => file.endsWith('.js')); 
+  for (let event_file of events) {
+  let event = require(`../events/${dir}/${event_file}`);
+  if (event.once == true) client.once(event.data.name, (client, ...args) => event.run(client, ...args)); 
+  else client.on(event.data.name, (... args) => event.run(client, ...args));
   };
 });
 },
